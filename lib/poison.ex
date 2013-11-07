97033454986dcc1153cf2aa895a2ca81ff068430
Devin Torres <devin@devintorr.es>
2013-11-06 20:01:48 -0600

    Further work

--- lib/poison.ex
+++ lib/poison.ex
@@ -1,19 +1,97 @@
 defmodule Poison do
-  @whitespace ' \n\t\r'
-
   def parse(string) when is_binary(string) do
-    value(string) |> elem(0)
+    { value, rest } = value(skip_whitespace(string))
+    if rest != "" or skip_whitespace(rest) != "", do: throw(:invalid)
+    value
   end
 
-  lc ws inlist @whitespace do
-    defp value(<< unquote(ws), rest :: binary >>), do: value(rest)
+  defp value("\"" <> rest),    do: string_start(rest)
+  defp value("{" <> rest),     do: object_start(rest)
+  defp value("[" <> rest),     do: array_start(rest)
+  defp value("null" <> rest),  do: { nil, rest }
+  defp value("true" <> rest),  do: { true, rest }
+  defp value("false" <> rest), do: { false, rest }
+
+  lc char inlist '-0123456789' do
+    defp value(<< unquote(char), rest :: binary >>) do
+      start_number(rest, unquote(char))
+    end
   end
 
-  defp value("\"" <> rest), do: enter_string(rest)
-  defp value("{"  <> rest), do: enter_object(rest)
+  defp value(_), do: throw(:invalid)
 
-  defp enter_string(rest) do
-    { iolist, rest } = parse_string(rest, "")
+  ## Numbers
+
+  defp start_number(<< ?0, _ :: binary >>, ?-) do
+    throw(:invalid)
+  end
+
+  lc char inlist '0123456789' do
+    defp start_number(<< unquote(char), _rest :: binary >>, ?0) do
+      throw(:invalid)
+    end
+
+    defp start_number(<< unquote(char), rest :: binary >>, first) do
+      number_int(rest, << first, unquote(char) >>)
+    end
+  end
+
+  defp start_number("", ?-), do: throw(:invalid)
+
+  defp start_number("", first) do
+    { first - ?0, "" }
+  end
+
+  lc char inlist '0123456789' do
+    defp number_int(<< unquote(char), rest :: binary >>, acc) do
+      number_int(rest, << acc :: binary, unquote(char) >>)
+    end
+  end
+
+  defp number_int(<< e, rest :: binary >>, acc) when e in 'eE' do
+    { exp, rest } = number_exp(rest, "")
+    { trunc(binary_to_float(<< acc :: binary, ".0e", exp :: binary >>)), rest }
+  end
+
+  defp number_int(<< ?., rest :: binary >>, acc) do
+    { frac, exp, rest } = number_frac(rest, "")
+    number = binary_to_float(<< acc :: binary, ?., frac :: binary, ?e, exp :: binary >>)
+    { number, rest }
+  end
+
+  defp number_int(rest, acc) do
+    { binary_to_integer(acc), rest }
+  end
+
+  lc char inlist '0123456789' do
+    defp number_frac(<< unquote(char), rest :: binary >>, acc) do
+      number_frac(rest, << acc :: binary, unquote(char) >>)
+    end
+  end
+
+  defp number_frac(<< e, rest :: binary >>, acc) when e in 'eE' do
+    { exp, rest } = number_exp(rest, "")
+    { acc, exp, rest }
+  end
+
+  defp number_frac(rest, acc) do
+    { acc, "0", rest }
+  end
+
+  lc char inlist '-0123456789' do
+    defp number_exp(<< unquote(char), rest :: binary >>, acc) do
+      number_exp(rest, << acc :: binary, unquote(char) >>)
+    end
+  end
+
+  defp number_exp(rest, acc) do
+    { acc, rest }
+  end
+
+  ## Strings
+
+  defp string_start(string) do
+    { iolist, rest } = parse_string(string, "")
     { iolist_to_binary(iolist), rest }
   end
 
@@ -21,37 +99,72 @@ defmodule Poison do
     { acc, rest }
   end
 
-  defp parse_string(<< char :: utf8, rest :: binary >>, acc) do
-    parse_string(rest, [acc, char])
+  defp parse_string(<< codepoint :: utf8, rest :: binary >>, acc) do
+    parse_string(rest, [acc, codepoint])
   end
 
-  defp enter_object(rest) do
-    pairs(rest, [])
+  ## Objects
+
+  defp object_start(string) do
+    object_pairs(skip_whitespace(string), [])
   end
 
-  lc ws inlist @whitespace do
-    defp pairs(<< unquote(ws), rest :: binary >>, acc), do: pairs(rest, acc)
+  defp object_pairs("\"" <> rest, acc) do
+    { name, rest } = string_start(rest)
+    { value, rest } = object_pair_value(skip_whitespace(rest))
+    maybe_leave_object(skip_whitespace(rest), [ { name, value } | acc ])
   end
 
-  defp pairs("\"" <> rest, acc) do
-    { name, rest } = enter_string(rest)
-    { value, rest } = pair_value(rest)
-    pairs(rest, [ { name, value } | acc])
+  defp object_pairs(_, _), do: throw(:invalid)
+
+  defp object_pair_value(":" <> rest) do
+    value(skip_whitespace(rest))
   end
 
-  defp pairs("," <> rest, acc) do
-    pairs(rest, acc)
+  defp object_pair_value(_), do: throw(:invalid)
+
+  defp maybe_leave_object("," <> rest, acc) do
+    object_pairs(skip_whitespace(rest), acc)
   end
 
-  defp pairs("}" <> rest, acc) do
+  defp maybe_leave_object("}" <> rest, acc) do
     { :lists.reverse(acc), rest }
   end
 
-  lc ws inlist @whitespace do
-    defp pair_value(<< unquote(ws), rest :: binary >>), do: pair_value(rest)
+  defp maybe_leave_object(_, _), do: throw(:invalid)
+
+  ## Arrays
+
+  defp array_start(string) do
+    array_values(string, [])
   end
 
-  defp pair_value(":" <> rest) do
-    value(rest)
+  defp array_values(string, acc) do
+    { value, rest } = value(skip_whitespace(string))
+    maybe_leave_array(rest, [ value | acc ])
+  end
+
+  defp maybe_leave_array("," <> rest, acc) do
+    array_values(rest, acc)
+  end
+
+  defp maybe_leave_array("]" <> rest, acc) do
+    { :lists.reverse(acc), rest }
+  end
+
+  defp maybe_leave_array(_, _), do: throw(:invalid)
+
+  ## Whitespace
+
+  defp skip_whitespace(""), do: ""
+
+  lc ws inlist ' \n\t\r' do
+    defp skip_whitespace(<< unquote(ws), rest :: binary >>) do
+      skip_whitespace(rest)
+    end
+  end
+
+  defp skip_whitespace(string) do
+    string
   end
 end

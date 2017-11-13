a20855416ddea6d1fdf67ab025bbf8e6e062b330
yuan.lei <lei.yuan@mixi.co.jp>
2017-11-13 16:19:22 +0900

    fix warning

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Poison.Mixfile do
   use Mix.Project
 
-  @version File.read!("VERSION") |> String.strip
+  @version File.read!("VERSION") |> String.trim
 
   def project do
     [app: :poison,

281a587dcfb815ddfa759ec4efb10dfad4916d50
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-06-16 17:02:46 +0100

    Avoid contract supertype Dialyzer -issued warning
    Dialyzer is too smart at figuring stuff out so we get:
    
    lib/poison.ex:73:contract_supertype
    Type specification is a supertype of the success typing.
    
    Function:
    Poison.decode!/1
    
    Type specification:
    @spec decode!(iodata()) :: Poison.Parser.t() | no_return()
    
    Success typing:
    @spec decode!(
      nonempty_binary()
      | maybe_improper_list(
          binary() | maybe_improper_list(any(), binary() | []) | byte(),
          binary() | []
        )
    ) ::
      false
      | nil
      | true
      | binary()
      | [false | nil | true | binary() | [any()] | number() | {_, _, _} | map()]
      | number()
      | %{
          :__struct__ =>
            Decimal | false | nil | true | binary() | [any()] | number() | {_, _, _} | map(),
          (atom() | binary() | {_, _, _}) =>
            false | nil | true | binary() | [any()] | number() | {_, _, _} | map()
        }
    
    which means only nonempty_binary | iolist (instead of iodata) would shut it up

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,6 @@
 [
   {"lib/poison/encoder.ex", :unknown_function, 80},
   {"lib/poison/decoder.ex", :unknown_function, 106},
-  {"lib/poison/parser.ex", :improper_list_constr}
+  {"lib/poison/parser.ex", :improper_list_constr},
+  {"lib/poison.ex", :contract_supertype}
 ]
